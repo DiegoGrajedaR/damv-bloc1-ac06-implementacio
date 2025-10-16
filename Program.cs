189c482359f0bb00ac714e3c31f6018cdce783f9
Diego <diego.grajeda.7e8@itb.cat>
2025-10-16 12:32:25 +0200

    Program.cs afegit a la branca Solution/SumTwoNumbers

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
 ﻿using System;
-namespace SumTwoNumbers
+
+public class HelloWorld
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            const string MsgInput1 = "Enter the first integer: ";
-            const string MsgInput2 = "Enter the second integer: ";
-            const string MsgResult = "The sum is: ";
-
-            int num1, num2, sum;
-
-            Console.WriteLine(MsgInput1);
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(MsgInput2);
-            num2 = Convert.ToInt32(Console.ReadLine());
-            sum = num1 + num2;
-            Console.WriteLine(MsgResult + sum);
-
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
