e4df3685ec228aa1590e4d7bf9cf8bdb080fae6b
Diego <diego.grajeda.7e8@itb.cat>
2025-10-16 23:01:10 +0200

    Solució exercici 5 --> calcular mitjana de tres nombres

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
-
-public class HelloWorld
+namespace AverageThreeNumbers
 {
-    public static void Main()
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main()
+        {
+            const string MsgInput1 = "Enter the first number: ";
+            const string MsgInput2 = "Enter the second number: ";
+            const string MsgInput3 = "Enter the third number: ";
+            const string MsgResult = "The average is: ";
+
+            double num1, num2, num3, average;
+
+            Console.WriteLine(MsgInput1);
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(MsgInput2);
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(MsgInput3);
+            num3 = Convert.ToDouble(Console.ReadLine());
+            average = (num1 + num2 + num3)/3;
+            Console.WriteLine(MsgResult + average);
+
+        }
     }
 }
